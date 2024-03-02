2cc4dcd15556112f6d89c41d06a3d9e896343c74
Mahmud <me@imahmud.com>
2024-03-02 17:28:42 +0600

    Build the search form

--- src/search-form/index.js
+++ src/search-form/index.js
@@ -1,11 +1,49 @@
 import { registerBlockType } from "@wordpress/blocks";
+import {
+  InspectorControls,
+  PanelColorSettings,
+  useBlockProps,
+} from "@wordpress/block-editor";
+import { __ } from "@wordpress/i18n";
 import block from "./block.json";
 import { searchIcon as icon } from "../icons";
+import "./main.css";
 
 registerBlockType(block.name, {
   icon,
-  edit: () => {
-    return <p>Hello World</p>;
+  edit: ({ attributes, setAttributes }) => {
+    const blockProps = useBlockProps();
+    const { bgColor, textColor } = attributes;
+    return (
+      <>
+        <InspectorControls>
+          <PanelColorSettings
+            title={__("Colors", "wp-block-dev")}
+            colorSettings={[
+              {
+                label: __("Background Color", "wp-block-dev"),
+                value: bgColor,
+                onChange: (newVal) => setAttributes({ bgColor: newVal }),
+              },
+              {
+                label: __("Text Color", "wp-block-dev"),
+                value: textColor,
+                onChange: (newVal) => setAttributes({ textColor: newVal }),
+              },
+            ]}
+          />
+        </InspectorControls>
+        <div class='wp-block-udemy-plus-search-form' {...blockProps}>
+          <h1>Search: Your search term here</h1>
+          <form>
+            <input type='text' placeholder='Search' />
+            <div class='btn-wrapper'>
+              <button type='submit'>Search</button>
+            </div>
+          </form>
+        </div>
+      </>
+    );
   },
   save: () => {
     return <p>Hello World</p>;
