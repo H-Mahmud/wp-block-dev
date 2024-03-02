d7de96dc358f2e6c1a7c8a784d1234f71ea7d571
Mahmud <me@imahmud.com>
2024-03-02 18:06:10 +0600

    Store editor data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import { __ } from "@wordpress/i18n";
 import block from "./block.json";
 
 registerBlockType(block.name, {
-  edit: () => {
-    return <RichText placeholder={__("Enter your heading", "wp-block-dev")} />;
+  edit: ({ attributes, setAttributes }) => {
+    const { content } = attributes;
+    return (
+      <RichText
+        onChange={(newVal) => setAttributes({ content: newVal })}
+        placeholder={__("Enter your heading", "wp-block-dev")}
+        value={content}
+      />
+    );
   },
 });
