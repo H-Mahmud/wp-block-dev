4bfe0a00ed8259bc85ac7b3384abd36bb916523a
Mahmud <me@imahmud.com>
2024-03-02 17:27:44 +0600

    Refactor: Multi-block file structure

--- inc/register-blocks.php
+++ inc/register-blocks.php
@@ -2,5 +2,13 @@
 defined('ABSPATH') ||exit;
 
 function wbd_register_blocks() {
-    register_block_type(WBD_DIR_PATH . 'build');
+
+    $blocks = [
+        "fancy-header"
+    ];
+
+    foreach($blocks as $block) {
+        register_block_type(WBD_DIR_PATH . 'build/' .$block);
+    }
+
 }
