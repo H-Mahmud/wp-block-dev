4d691baef55d85a4ce4ee54ffd6cd895aa713396
Mahmud <me@imahmud.com>
2024-03-02 17:44:13 +0600

    Oputput the search form

--- inc/search-form-renderer.php
+++ inc/search-form-renderer.php
@@ -2,5 +2,23 @@
 defined('ABSPATH') || exit;
 
 function wbd_search_form_renderer() {
-    return '<h1>Hello</h1>';
+    ob_start(); ?>
+    <div {...blockProps}>
+          <h1>Search: Your search term here</h1>
+          <form>
+            <input type='text' placeholder='Search' />
+            <div class='btn-wrapper'>
+              <button
+                type='submit'
+                style={{ background: bgColor, color: textColor }}
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+        <?php
+        $content = ob_get_contents();
+        ob_clean();
+        return $content;
 }
